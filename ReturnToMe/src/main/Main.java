ef625de7521cff87d0e5669a1abc105271274eb0
schalox <user@fake>
2017-02-13 15:47:55 +0200

    Rearranging main

--- ReturnToMe/src/main/Main.java
+++ ReturnToMe/src/main/Main.java
@@ -11,15 +11,18 @@ import functions.Follow;
 public class Main {
 	public static void main(String[] args) {
 		IR sensori = new IR(SensorPort.S1);
-		sensori.start();
 		Print printer = new Print(sensori);	
-		printer.start();
 		Movement liike = new Movement(MotorPort.A, MotorPort.D, 360);
 		Follow follower = new Follow(sensori, liike);
+		
+		sensori.start();
+		printer.start();
 		follower.start();
+		
 		while (Button.ESCAPE.isUp()) {
 			Delay.msDelay(50);
 		}
+		
 		sensori.setStop();
 		printer.setStop();
 		follower.setStop();
