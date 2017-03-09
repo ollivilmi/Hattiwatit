7393f6660984cceea48c2ccc056426dfb023560f
Hillo <Hillo@DESKTOP-UOAUN28>
2017-03-09 18:02:17 +0200

    Added tail, added tail to PatrolController

--- Hattiwatit/src/main/Main.java
+++ Hattiwatit/src/main/Main.java
@@ -5,7 +5,7 @@ import lejos.hardware.port.SensorPort;
 
 public class Main {
 	public static void main(String[] args) {
-		Doge doge = new Doge(SensorPort.S3, SensorPort.S2, MotorPort.D, MotorPort.A);
+		Doge doge = new Doge(SensorPort.S3, SensorPort.S2, MotorPort.D, MotorPort.A, SensorPort.S1, MotorPort.B);
 		doge.start();
 	}
 }
