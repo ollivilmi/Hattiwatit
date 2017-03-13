ae117cdbc300e9fc0a69b14b5359c8f724ac42d5
schalox <user@fake>
2017-03-13 11:04:05 +0200

    Menu: add documentation

--- Hattiwatit/src/main/Menu.java
+++ Hattiwatit/src/main/Menu.java
@@ -3,6 +3,9 @@ package main;
 import lejos.hardware.lcd.LCD;
 import lejos.utility.TextMenu;
 
+/**
+ * Stores a menu and its items.
+ */
 public class Menu {
 	private TextMenu menu;
 
@@ -10,6 +13,13 @@ public class Menu {
 		menu = new TextMenu(menuItems, 1, menuTitle);
 	}
 
+	/**
+	 * Shows the menu in the Doge's LCD screen, lets the user to select an item,
+	 * and returns the item's index. Clears the screen after the selection has
+	 * been made.
+	 * 
+	 * @return Zero-based index of the selected item.
+	 */
 	public int showMenu() {
 		int selection = menu.select();
 		LCD.clear();
